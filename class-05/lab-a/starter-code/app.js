5bc92aa98bd4c7bf884d7bf91056182540a60410
afnanshihab <afnan.shihab@yahoo.com>
2021-06-12 20:57:56 +0300

    p5 is done

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -109,10 +109,16 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
 
+  var total =1; 
+  for (let i = 0; i < testArray.length; i++) {
+    total *= testArray[i];
+    
+  }
+  return [total , `The numbers 2,3,4 have a product of 24.`]
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
